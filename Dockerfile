d27ecf574adb071b775f6c4e9c0a39450e602cb8
Daniel Schröder <daniel.schroeder@skriptfabrik.com>
2022-07-11 14:06:53 +0200

    Update to Elements 7.6.0

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL maintainer="Daniel Schröder <daniel.schroeder@skriptfabrik.com>"
 
 ARG ELEMENTS_CLI_VERSION=latest
 
-ENV HOSTNAME=0.0.0.0
+ENV ELEMENTS_HOSTNAME=0.0.0.0
 ENV NODE_ENV=production
 
 COPY . /opt/elements-cli-${ELEMENTS_CLI_VERSION}
--- elements-cli.js
+++ elements-cli.js
@@ -18,13 +18,13 @@ const { URL } = require('url');
 // Argument defaults
 
 const argd = {
-  'base-path': process.env.BASE_PATH || '/',
-  hostname: process.env.HOSTNAME || 'localhost',
-  layout: process.env.LAYOUT || 'sidebar',
-  port: parseInt(process.env.PORT || '8000'),
-  router: process.env.ROUTER || 'history',
-  style: process.env.STYLE || 'display: block; height: 100vh',
-  title: process.env.TITLE || 'My API Docs',
+  'base-path': process.env.ELEMENTS_BASE_PATH || process.env.BASE_PATH  || '/',
+  hostname: process.env.ELEMENTS_HOSTNAME || 'localhost',
+  layout: process.env.ELEMENTS_LAYOUT || process.env.LAYOUT || 'sidebar',
+  port: parseInt(process.env.ELEMENTS_PORT || '8000'),
+  router: process.env.ELEMENTS_ROUTER || process.env.ROUTER || 'history',
+  style: process.env.ELEMENTS_STYLE || process.env.STYLE || 'flex: 1 0 0; overflow: hidden;',
+  title: process.env.ELEMENTS_TITLE || process.env.TITLE  || 'My API Docs',
   'working-dir': process.cwd(),
 };
 
